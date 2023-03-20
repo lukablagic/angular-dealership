3620c547746c5e2acd2c8787ac2e1dece6667113
unknown <blagicluka666@gmail.com>
2023-03-20 14:40:10 +0100

    home page filter adding/redesign

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -21,50 +21,22 @@ export class NavbarComponent {
   languages: Language[];
   selectedLanguage: Language;
   visibleSidebar1: boolean = false;
-  visibleSidebar2: boolean = false;
-  visibleSidebar3: boolean = false;
-  visibleSidebar4: boolean = false;
 
   constructor(private primengConfig: PrimeNGConfig) {}
 
   ngOnInit() {
     this.items = [
-      { label: 'Mercedes-Benz', routerLink: ['/home']},
-      { label: 'Models', icon: 'pi pi-fw pi-home' , command: () => this.visibleSidebar1 = true },
-      { label: 'Search and Buy', icon: 'pi pi-fw pi-shopping-cart' , command: () => this.visibleSidebar2 = true },
-      { label: 'Inovations', icon: 'pi pi-fw pi-phone',
-      command: () => this.visibleSidebar3 = true  },
-      { label: 'Service', icon: 'pi pi-fw pi-info', command: () => this.visibleSidebar4 = true },
+      { label: 'Best Cars', routerLink: ['/home']},
+      { label: 'Search and Buy', icon: 'pi pi-fw pi-shopping-cart' ,routerLink: ['/search-buy']},
+      { label: 'Service and Repair', icon: 'pi pi-fw pi-info', routerLink: ['/service'] },
+      { label: 'News and Rewiews', icon: 'pi pi-fw pi-info',routerLink: ['/news'] },
+      { label: 'Sell your Car', icon: 'pi pi-fw pi-info',routerLink: ['/sell-car'] },
+      { label: 'Addd cars', icon: 'pi pi-fw pi-info',routerLink: ['/addcars'] },
     ];
     this.primengConfig.ripple = true;
     this.languages = [
       { name: 'EN', code: 'EN' },
       { name: 'DE', code: 'DE' },
-    ];
-    this.itemsModles = [
-      {label: 'A-Class', icon: 'pi pi-fw pi-home'},
-      {label: 'B-Class', icon: 'pi pi-fw pi-home'},
-      {label: 'C-Class', icon: 'pi pi-fw pi-home'},
-      {label: 'E-Class', icon: 'pi pi-fw pi-home'},
-      {label: 'S-Class', icon: 'pi pi-fw pi-home'},
-      {label: 'V-Class', icon: 'pi pi-fw pi-home'},
-      {label: 'G-Class', icon: 'pi pi-fw pi-home'},
-    ];
-    this.itemsSearch = [
-    {label: 'New Cars', icon: 'pi pi-fw pi-home', routerLink: ['/search-buy'],command: () => this.visibleSidebar2 = false},
-    {label:'Used Cars', icon: 'pi pi-fw pi-home', routerLink: ['/search-buy'],command: () => this.visibleSidebar2 = false},
-    {label:'Build Your Own', icon: 'pi pi-fw pi-home' ,routerLink: ['/custom-build'],command: () => this.visibleSidebar2 = false},
-    {label:'Find a Dealer', icon: 'pi pi-fw pi-home' ,routerLink: ['/find-dealer'],command: () => this.visibleSidebar2 = false },
-    ];
-    this.itemsInovations = [
-    {label: 'Electric Vehicles', icon: 'pi pi-fw pi-home'},
-    {label: 'Hybrid Vehicles', icon: 'pi pi-fw pi-home'},
-    {label: 'Plug-in Hybrid Vehicles', icon: 'pi pi-fw pi-home'},
-    ];
-    this.itemsService = [
-    {label:"Actueal Offers", icon: 'pi pi-fw pi-home'},
-    {label:"Mercedes-Benz Service", icon: 'pi pi-fw pi-home'},
-    {label:"Mercedes-Benz Financial Services", icon: 'pi pi-fw pi-home'},
     ];
 
   }
--- src/app/shared/components/filters/filters.component.ts
+++ src/app/shared/components/filters/filters.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Car } from '../../models/car.model';
+import { Equipment } from '../../models/equipment.model';
 
 @Component({
   selector: 'app-filters',
@@ -6,5 +9,117 @@ import { Component } from '@angular/core';
   styleUrls: ['./filters.component.scss']
 })
 export class FiltersComponent {
+  @Output() carEventEmmiter = new EventEmitter<Car>();
 
+  carForm: FormGroup = new FormGroup({
+    model: new FormControl(''),
+    // carPrice: new FormControl('')
+  });
+  models: any[];
+  priceRange: any[]= [
+    { label: '5000', value: 5000 },
+    { label: '10000', value: 10000 },
+    { label: '15000', value: 15000 },
+    { label: '20000', value: 20000 },
+    { label: '25000', value: 25000 },
+    { label: '30000', value: 30000 },
+];
+  selectedPriceRange: number;
+  mileageRange = [
+    { label: '50000', value: 50000 },
+    { label: '100000', value: 100000 },
+    { label: '150000', value: 150000 },
+    { label: '200000', value: 200000 },
+];
+  selectedMileageRange: number;
+  yearRange: any[] = [
+    { label: '2010', value: 2010 },
+    { label: '2011', value: 2011 },
+    { label: '2012', value: 2012 },
+    { label: '2013', value: 2013 },
+    { label: '2014', value: 2014 },
+    { label: '2015', value: 2015 },
+    { label: '2016', value: 2016 },
+    { label: '2017', value: 2017 },
+    { label: '2018', value: 2018 },
+    { label: '2019', value: 2019 },
+    { label: '2020', value: 2020 },
+];
+  selectedYearRange: number; 
+transmissionRange: any[] = [
+    { label: 'RWD', value: 'RWD' },
+    { label: 'FWD', value: 'FWD' },
+    { label: '4x4', value: '4x4' },
+];
+  selectedTransmissionRange: string;
+  fuelRange: any[] = [
+    { label: 'Petrol', value: 'Petrol' },
+    { label: 'Diesel', value: 'Diesel' },
+    { label: 'Hybrid', value: 'Hybrid' },
+    { label: 'Electric', value: 'Electric' },
+];
+selectedFuelRange: string;
+  val2: number = 50;
+  engines: String[];
+  selectedModel: Car;
+  carYear: number;
+  carMileage: number;
+  carFuel: string;
+  carTransmission: string;
+  selectedMileage: number = 0;
+  engineSize: number;
+  engineSizeOptions: number[];
+  selectedEngine: number;
+  equipment: Equipment;
+  engineType: string;
+  car: Car;
+  display: boolean = false;
+  airConditioning: boolean;
+  cruiseControl: boolean;
+  parkingSensors: boolean;
+  powerSteering: boolean;
+  powerWindows: boolean;
+  powerMirrors: boolean;
+  ABS: boolean;
+  airbags: boolean;
+
+
+  showDialog() {
+      this.display = true;
+  }
+
+
+  constructor() {
+    this.models = [
+      { model: 'S Class' },
+      { model: 'A Class' },
+      { model: 'E Class' },
+      { model: 'C Class' },
+      { model: 'B Class' },
+      { model: 'V Class' },
+      { model: 'GLA Class' },
+      { model: 'GLC Class' },
+      { model: 'GLE Class' },
+      { model: 'GLS Class' },
+      { model: 'G Class' },
+    ];
+
+    this.selectedModel = new Car();
+    this.car = new Car();
+  }
+
+  sendCarData(car: Car) {
+    this.car.model = this.selectedModel.model;
+    this.car.price = this.selectedPriceRange;
+    this.car.year = this.carYear;
+    this.car.mileage = this.carMileage;
+    this.carMileage = this.selectedMileage;
+    this.car.fuel = this.carFuel;
+    this.car.transmission = this.carTransmission;
+    this.car.engineSize = this.selectedEngine;
+    this.equipment = new Equipment(this.airConditioning, this.cruiseControl, this.parkingSensors, this.powerSteering, this.powerWindows, this.powerMirrors, this.ABS, this.airbags);
+    this.car.equipment = this.equipment;
+    
+    this.carEventEmmiter.emit(car);
+  }
 }
